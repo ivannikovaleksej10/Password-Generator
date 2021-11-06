fe64992689e1cab46debf4c02ede16d2cc4e64be
lauendewrau <77196217+lauendewrau@users.noreply.github.com>
2021-11-06 20:58:00 +0700

    Добавлено шифрование/дешифрование пароля, сохранение зашифрованного пароля в текстовый документ с отдельным расширением

---
+++ Save.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace Passwords
+{
+    partial class Save
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TbValue = new System.Windows.Forms.TextBox();
+            this.TbEncrypt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TbDecrypt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.HeaderPanel = new System.Windows.Forms.Panel();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.FormName = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.HeaderPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Значение:";
+            // 
+            // TbValue
+            // 
+            this.TbValue.Location = new System.Drawing.Point(163, 11);
+            this.TbValue.Name = "TbValue";
+            this.TbValue.Size = new System.Drawing.Size(264, 20);
+            this.TbValue.TabIndex = 1;
+            // 
+            // TbEncrypt
+            // 
+            this.TbEncrypt.Location = new System.Drawing.Point(163, 37);
+            this.TbEncrypt.Name = "TbEncrypt";
+            this.TbEncrypt.Size = new System.Drawing.Size(264, 20);
+            this.TbEncrypt.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Зашифрованное значение:";
+            // 
+            // TbDecrypt
+            // 
+            this.TbDecrypt.BackColor = System.Drawing.SystemColors.Window;
+            this.TbDecrypt.Location = new System.Drawing.Point(163, 63);
+            this.TbDecrypt.Name = "TbDecrypt";
+            this.TbDecrypt.ReadOnly = true;
+            this.TbDecrypt.Size = new System.Drawing.Size(264, 20);
+            this.TbDecrypt.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Расшифрованное значение:";
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.FlatAppearance.BorderSize = 0;
+            this.DecryptButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DecryptButton.Location = new System.Drawing.Point(333, 89);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(94, 23);
+            this.DecryptButton.TabIndex = 6;
+            this.DecryptButton.Text = "Расшифровать";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.FlatAppearance.BorderSize = 0;
+            this.EncryptButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EncryptButton.Location = new System.Drawing.Point(233, 89);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(94, 23);
+            this.EncryptButton.TabIndex = 7;
+            this.EncryptButton.Text = "Зашифровать";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.FlatAppearance.BorderSize = 0;
+            this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveButton.Location = new System.Drawing.Point(133, 89);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(94, 23);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.TbValue);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.TbEncrypt);
+            this.panel1.Controls.Add(this.SaveButton);
+            this.panel1.Controls.Add(this.TbDecrypt);
+            this.panel1.Controls.Add(this.DecryptButton);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.EncryptButton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(442, 120);
+            this.panel1.TabIndex = 9;
+            // 
+            // HeaderPanel
+            // 
+            this.HeaderPanel.BackColor = System.Drawing.Color.Transparent;
+            this.HeaderPanel.Controls.Add(this.CloseButton);
+            this.HeaderPanel.Controls.Add(this.FormName);
+            this.HeaderPanel.Location = new System.Drawing.Point(0, 0);
+            this.HeaderPanel.Name = "HeaderPanel";
+            this.HeaderPanel.Size = new System.Drawing.Size(442, 32);
+            this.HeaderPanel.TabIndex = 10;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.Transparent;
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.CloseButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(67)))), ((int)(((byte)(77)))));
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CloseButton.ForeColor = System.Drawing.Color.White;
+            this.CloseButton.Location = new System.Drawing.Point(412, 0);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(30, 30);
+            this.CloseButton.TabIndex = 11;
+            this.CloseButton.Text = "✕";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            // 
+            // FormName
+            // 
+            this.FormName.AutoSize = true;
+            this.FormName.BackColor = System.Drawing.Color.Transparent;
+            this.FormName.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormName.ForeColor = System.Drawing.Color.White;
+            this.FormName.Location = new System.Drawing.Point(3, 9);
+            this.FormName.Name = "FormName";
+            this.FormName.Size = new System.Drawing.Size(113, 16);
+            this.FormName.TabIndex = 12;
+            this.FormName.Text = "Save and encrypt";
+            // 
+            // Save
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 154);
+            this.Controls.Add(this.HeaderPanel);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Save";
+            this.Text = "Save";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.HeaderPanel.ResumeLayout(false);
+            this.HeaderPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TbEncrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TbDecrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel HeaderPanel;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Label FormName;
+        public System.Windows.Forms.TextBox TbValue;
+    }
+}
---
+++ Save.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Passwords
+{
+    public partial class Save : FormShadow
+    {
+        public Save()
+        {
+            InitializeComponent();
+
+            CloseButton.Click += (s, a) => this.Close();
+
+            FormPaint(Color.FromArgb(44, 57, 67), Color.FromArgb(35, 44, 55));
+
+            new List<Control> { FormName, HeaderPanel }.ForEach(x =>
+            {
+                x.MouseDown += (s, a) =>
+                {
+                    x.Capture = false;
+                    Capture = false;
+                    Message m = Message.Create(Handle, 0xA1, new IntPtr(2), IntPtr.Zero);
+                    base.WndProc(ref m);
+                };
+            });
+
+            new List<Control> { label1, label2, label3, SaveButton, EncryptButton, DecryptButton }.ForEach(x =>
+            {
+                x.ForeColor = Color.FromName("White");
+                if (x.ToString().Contains("u"))
+                {
+                    x.BackColor = Color.FromArgb(44, 57, 67);
+                }
+            });
+
+        }
+
+        
+
+        public void FormPaint(Color color1, Color color2)
+        {
+            void OnPaintEventHandler(object s, PaintEventArgs a)
+            {
+                if (ClientRectangle == Rectangle.Empty)
+                    return;
+
+                var lgb = new LinearGradientBrush(ClientRectangle, Color.Empty, Color.Empty, 90);
+                var cblend = new ColorBlend { Colors = new[] { color1, color1, color2, color2 }, Positions = new[] { 0, 0.195f, 0.195f, 1 } };
+
+                lgb.InterpolationColors = cblend;
+                a.Graphics.FillRectangle(lgb, ClientRectangle);
+            }
+
+            Paint -= OnPaintEventHandler;
+            Paint += OnPaintEventHandler;
+
+            Invalidate();
+        }
+
+        readonly string hash = "501ad03e7f7de4207cff7ba83d23330cfc0df4c6";
+
+        void EncryptButton_Click(object sender, EventArgs e)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(TbValue.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    TbEncrypt.Text = Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+        }
+
+        void DecryptButton_Click(object sender, EventArgs e)
+        {
+            byte[] data = Convert.FromBase64String(TbEncrypt.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    TbDecrypt.Text = UTF8Encoding.UTF8.GetString(results);
+                }
+            }
+        }
+
+        void SaveButton_Click(object sender, EventArgs e)
+        {
+            DateTime thisDay = DateTime.Today;
+
+            using (SaveFileDialog saveFileDialog1 = new SaveFileDialog())
+            {
+                saveFileDialog1.InitialDirectory = Convert.ToString(Environment.SpecialFolder.MyDocuments);
+                saveFileDialog1.Filter = "Encrypt Passwords (*.enps)|*.enps|All Files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 1;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        string path = saveFileDialog1.FileName;
+                        FileStream fs = new FileStream(path, FileMode.Append);
+                        using (StreamWriter sw = new StreamWriter(fs))
+                        {
+                            sw.WriteLine(TbEncrypt.Text + "    " + thisDay.ToString("D"));
+                        }
+                    }
+            }
+        }
+    }
+}
